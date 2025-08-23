68a912c548e48de0180d8cd556cd7e80636de7d0
tracer-ysyx <tracer@ysyx.org>
2025-08-24 01:59:07 +0800

    > compile NEMU ysyx_22040000 李卓恒 Linux adan 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:59:07 up  2:45,  1 user,  load average: 0.01, 0.02, 0.00

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -22,9 +22,6 @@ static const uint32_t img [] = {
   0x00000513, // addi a0,x0,0
   0x00150513, // addi a0,a0,1        a0:0->1 
   0x00250513, // addi a0,a0,2        a0:1->3 
-  0x00000297,  // auipc t0,0
-  0x00028823,  // sb  zero,16(t0)
-  0x0102c503,  // lbu a0,16(t0)
   0x00100073,  // ebreak (used as nemu_trap)
   0xdeadbeef,  // some data
 };
