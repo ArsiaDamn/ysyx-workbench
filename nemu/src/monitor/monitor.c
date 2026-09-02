c572e869c6914ef4dfef4d70c37d658ae8c5d7bd
26010001-Li Zhuoheng <202401201111@stumail.sztu.edu.cn>
2026-09-02 22:52:19 +0800

    ftrace has been finished.

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -37,14 +37,100 @@ static void welcome() {
 }
 
 #ifndef CONFIG_TARGET_AM
-#include <getopt.h>
+  #include <getopt.h>
+  #ifdef CONFIG_FTRACE
+    #include <elf.h>      
+  #endif
 
 void sdb_set_batch_mode();
 
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
+static char *elf_file = NULL;
 static int difftest_port = 1234;
+
+#ifdef CONFIG_FTRACE
+  typedef struct {
+    word_t value;
+    word_t size;
+    const char *name;
+  } FuncSymbol;
+  static FuncSymbol *funcs = NULL;
+  static int func_cnt = 0;
+  static int func_depth = 0;
+
+  static void init_ftrace(const char *elf_file){
+    if (elf_file == NULL) {
+      Log("ftrace: No ELF file is given. .");
+      return;
+    }
+    FILE *fp = fopen(elf_file, "rb");
+    Assert(fp, "ftrace can not open '%s'", elf_file);
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    
+    uint8_t *elf_buf = malloc(size);
+    assert(elf_buf);
+    ret = fread(elf_buf, size, 1, fp);
+    assert(ret == 1);
+    fclose(fp);
+
+    Elf32_Ehdr *ehdr = (Elf32_Ehdr *)elf_buf;
+    Assert(memcmp(ehdr->e_ident, ELFMAG, SELFMAG) == 0, "ftrace:%s is not a valid ELF file", elf_file);
+    Elf32_Shdr *shdr = (Elf32_Shdr *)(elf_buf + ehdr->e_shoff);
+    Elf32_Shdr *symtab = NULL;
+    Elf32_Shdr *strtab = NULL;
+    for (int i = 0; i < ehdr->e_shnum; i ++) {
+      if (shdr[i].sh_type == SHT_SYMTAB) {
+        symtab = &shdr[i];
+        strtab = &shdr[symtab->sh_link];
+        break;
+      }
+    }
+    Assert(symtab != NULL, "ftrace: no symbol table in '%s'", elf_file);
+
+    Elf32_Sym *sym = (Elf32_Sym *)(elf_buf + symtab->sh_offset);
+    int symtab_cnt = symtab->sh_size / sizeof(Elf32_Sym);
+    const char *strtab_base = (const char *)(elf_buf + strtab->sh_offset);
+    funcs = malloc(symtab_cnt * sizeof(FuncSymbol));
+    for (int i = 0; i < symtab_cnt; i ++) {
+      if (ELF32_ST_TYPE(sym[i].st_info) == STT_FUNC) {
+        funcs[func_cnt].value = sym[i].st_value;
+        funcs[func_cnt].size  = sym[i].st_size;
+        funcs[func_cnt].name  = strtab_base + sym[i].st_name;
+        func_cnt ++;
+      }
+    }
+    Log("ftrace: loaded %d function symbols from %s", func_cnt, elf_file);
+  }
+  
+  static const char *find_func(word_t addr, word_t *value) {
+    for (int i = 0; i < func_cnt; i ++) {
+      if (addr >= funcs[i].value && addr < funcs[i].value + funcs[i].size) {
+        *value = funcs[i].value;
+        return funcs[i].name;
+      }
+    }
+    return NULL;
+  }
+  void ftrace_call(vaddr_t pc, vaddr_t target) {
+    word_t value = target;
+    const char *name = find_func(target, &value);
+    if (name == NULL) name = "???";
+    log_write(FMT_WORD ": %*scall [%s@" FMT_WORD "]\n", pc, func_depth * 2, "", name, value);
+    func_depth ++;
+  }
+  void ftrace_ret(vaddr_t pc) {
+    if (func_depth > 0) func_depth --;
+    word_t value = pc;
+    const char *name = find_func(pc, &value);
+    if (name == NULL) name = "???";
+    log_write(FMT_WORD ": %*sret  [%s]\n", pc, func_depth * 2, "", name);
+  }
+#endif
+
 
 static long load_img() {
   if (img_file == NULL) {
@@ -73,14 +159,16 @@ static int parse_args(int argc, char *argv[]) {
     {"batch"    , no_argument      , NULL, 'b'},
     {"log"      , required_argument, NULL, 'l'},
     {"diff"     , required_argument, NULL, 'd'},
+    {"elf"      , required_argument, NULL, 'e'},
     {"port"     , required_argument, NULL, 'p'},
     {"help"     , no_argument      , NULL, 'h'},
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "-bhl:d:e:p:h:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
+      case 'e': elf_file = optarg; break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
@@ -90,6 +178,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-b,--batch              run with batch mode\n");
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+        printf("\t-e,--elf=ELF_FILE        ELF file for ftrace\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
         printf("\n");
         exit(0);
@@ -118,6 +207,9 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Perform ISA dependent initialization. */
   init_isa();
+
+  /* Initialize ftrace. */
+  IFDEF(CONFIG_FTRACE, init_ftrace(elf_file));
 
   /* Load the image to memory. This will overwrite the built-in image. */
   long img_size = load_img();
